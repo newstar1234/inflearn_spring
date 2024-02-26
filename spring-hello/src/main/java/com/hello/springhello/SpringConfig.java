81bbe7af5536cc375cf07845c1034ae1f9c20ac8
newstar <maori36@naver.com>
2024-02-26 23:42:38 +0900

    24.02.26

--- spring-hello/src/main/java/com/hello/springhello/SpringConfig.java
+++ spring-hello/src/main/java/com/hello/springhello/SpringConfig.java
@@ -1,17 +1,27 @@
 package com.hello.springhello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.hello.springhello.repository.JdbcMemberRepository;
 import com.hello.springhello.repository.MemberRepository;
 import com.hello.springhello.repository.MemoryMemberRepository;
 import com.hello.springhello.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
     
-    // 자바 코드로 직접 스프링 빈 등록하기 // 
+    private DataSource dataSource;
 
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // 자바 코드로 직접 스프링 빈 등록하기 // 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -19,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
