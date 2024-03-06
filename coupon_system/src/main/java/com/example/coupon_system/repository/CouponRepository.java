e388c37a98129350a357374a98a8886aa8016fbc
newstar <maori36@naver.com>
2024-03-06 16:21:59 +0900

    24.03.06 정리

--- coupon_system/src/main/java/com/example/coupon_system/repository/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.coupon_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.coupon_system.domain.Coupon;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    
-}
