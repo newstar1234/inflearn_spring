c523fbacd15e4091d2b3ec8349ff68c8f2a85716
newstar <maori36@naver.com>
2024-03-07 18:43:57 +0900

    24.03.07

---
+++ java/java_start/src/variable/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long longVariable = 10000000000L;
+
+        System.out.println("longVariable : " + longVariable);
+        
+        boolean booleanVariable = true;
+
+        System.out.println("booleanVariable : " + booleanVariable);
+    }
+
+}
