91623a893b6c05726c30cf850c892780e275376e
newstar <maori36@naver.com>
2024-03-16 17:39:26 +0900

    24.03.16

---
+++ java/java_start/src/loop/ex4.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class ex4 {
+    public static void main(String[] args) {
+        int num = 2;
+        for(int count =1; count <=10; count++ ){
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
---
+++ java/java_start/src/loop/ex6.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class ex6 {
+    public static void main(String[] args) {
+        
+        int max = 100;
+        int sum =0;
+
+        for(int i =1; i <= max; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+    }
+}
