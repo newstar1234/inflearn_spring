95df29e99dc6bf98d6c1d42356ad507b69b71ab2
newstar <maori36@naver.com>
2024-04-04 21:36:15 +0900

    24.04.04

--- product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -7,47 +7,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-
-    @BeforeEach
-    void setUp() {
-        productPort = Mockito.mock(ProductPort.class);
-        productService = new ProductService(productPort);
-
-    }
 
     @Test
     void 상품수정() {
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품수정", 2000, DiscountPolicy.NONE);
-        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-      
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
-        productService.updateProduct(productId, request);    
+        productService.updateProduct(productId, request);
 
-        assertThat(product.getName()).isEqualTo("상품수정");
-        assertThat(product.getPrice()).isEqualTo(2000);
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+
+        assertThat(productResponse.name()).isEqualTo("상품수정");
+        assertThat(productResponse.price()).isEqualTo(2000);
     }
-
-    // private static class StubProductPort implements ProductPort {
-
-    //     public Product getProduct_will_return;
-
-    //     @Override
-    //     public void save(final Product product) {
-            
-    //     } 
-
-    //     @Override
-    //     public Product getProduct(final Long productId) {
-    //         return getProduct_will_return;
-    //     }
-    // }
 
 }
