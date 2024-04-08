e2fde7948e056e9a4d0916bbe739a0376624bbfd
newstar <maori36@naver.com>
2024-04-08 12:43:05 +0900

    24.04.08

---
+++ product-order-service/src/main/java/com/example/productorderservice/order/OrderPort.java
@@ -0,0 +1,8 @@
+package com.example.productorderservice.order;
+
+import com.example.productorderservice.product.Product;
+
+interface OrderPort {
+    Product getProductById(final Long productId);
+    void save(final Order order);
+}
---
+++ product-order-service/src/main/java/com/example/productorderservice/order/OrderService.java
@@ -0,0 +1,24 @@
+package com.example.productorderservice.order;
+
+import org.springframework.stereotype.Component;
+
+import com.example.productorderservice.product.Product;
+
+@Component
+class OrderService {
+
+    private final OrderPort orderPort;
+
+    OrderService(final OrderPort orderPort) {
+        this.orderPort = orderPort;
+    }
+
+    public void createOrder(final CreateOrderRequest request) {
+        final Product product = orderPort.getProductById(request.productId());
+
+        final Order order = new Order(product, request.quantity());
+
+        orderPort.save(order);
+    }
+    
+}
