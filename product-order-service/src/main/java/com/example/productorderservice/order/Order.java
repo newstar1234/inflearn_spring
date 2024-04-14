b8d04e20b564dc9d8f3d8f145c8f9fefd3a1879d
newstar <maori36@naver.com>
2024-04-14 17:21:08 +0900

    24.04.14

--- product-order-service/src/main/java/com/example/productorderservice/order/Order.java
+++ product-order-service/src/main/java/com/example/productorderservice/order/Order.java
@@ -1,26 +1,40 @@
 package com.example.productorderservice.order;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.util.Assert;
 
 import com.example.productorderservice.product.Product;
 
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
+
+    @OneToOne
+    private Product product;
+    
+    private int quantity;
 
     public Order(final Product product, final int quantity){
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "수량은 0보다 커야합니다.");
         this.product = product;
         this.quantity = quantity;
-    }
-
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
--- product-order-service/src/main/java/com/example/productorderservice/order/OrderRepository.java
+++ product-order-service/src/main/java/com/example/productorderservice/order/OrderRepository.java
@@ -1,18 +1,7 @@
 package com.example.productorderservice.order;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
-class OrderRepository {
-    private final Map<Long, Order> persistence = new HashMap<>();
-    private Long sequence = 0L;
-    
-    public void save(final Order order) {
-        order.assignId(++sequence);
-        persistence.put(order.getId(), order);
-    }
+interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
